cac8924737e91d80f13a7dda62c67d4ff690bca9
Shubam Walia <37077495+shubamwalia@users.noreply.github.com>
2018-10-24 13:11:46 +0530

    Add files via upload

--- attendance/recognizer.py
+++ attendance/recognizer.py
@@ -1,13 +1,15 @@
 import cv2, numpy as np;
 import xlwrite,firebase.firebase_ini as fire;
 import time
+import sys
+from playsound import playsound
 start=time.time()
 period=8
-face_cas = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cas = cv2.CascadeClassifier('haarcascade_profileface.xml')
 cap = cv2.VideoCapture(0);
 recognizer = cv2.face.LBPHFaceRecognizer_create();
 recognizer.read('trainer/trainer.yml');
-
+flag = 0;
 id=0;
 filename='filename';
 dict = {
@@ -23,25 +25,44 @@ while True:
         roi_gray = gray[y:y + h, x:x + w]
         cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0),2);
         id,conf=recognizer.predict(roi_gray)
-        
-        if(id==1):
+        if(conf < 50):
+         if(id==1):
             id='Shubam Walia'
             if((str(id)) not in dict):
                 filename=xlwrite.output('attendance','class1',1,id,'yes');
                 dict[str(id)]=str(id);
                 
-        elif(id==2):
-            id = 'Srishti Sharma'
+         elif(id==2):
+            id = 'Rohan'
             if ((str(id)) not in dict):
                 filename =xlwrite.output('attendance', 'class1', 2, id, 'yes');
                 dict[str(id)] = str(id);
 
-                
+         elif(id==3):
+            id = 'Raveen'
+            if ((str(id)) not in dict):
+                filename =xlwrite.output('attendance', 'class1', 3, id, 'yes');
+                dict[str(id)] = str(id);
+
+         elif(id==4):
+            id = 'Sonu'
+            if ((str(id)) not in dict):
+                filename =xlwrite.output('attendance', 'class1', 4, id, 'yes');
+                dict[str(id)] = str(id);
+
+        else:
+             id = 'Unknown, can not recognize'
+             flag=flag+1
+             break
         
         cv2.putText(img,str(id)+" "+str(conf),(x,y-10),font,0.55,(120,255,120),1)
         #cv2.cv.PutText(cv2.cv.fromarray(img),str(id),(x,y+h),font,(0,0,255));
     cv2.imshow('frame',img);
     #cv2.imshow('gray',gray);
+    if flag == 10:
+        playsound('transactionSound.mp3')
+        print("Transction Blocked")
+        break;
     if time.time()>start+period:
         break;
     if cv2.waitKey(100) & 0xFF == ord('q'):
